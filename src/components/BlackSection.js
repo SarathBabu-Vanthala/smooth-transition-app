28559af9bdc0ada038bc40a75186f481912900d2
SarathBabu-Vanthala <sarathbabuvanthala@gmail.com>
2025-06-20 23:00:56 +0530

    first commit

---
+++ src/components/BlackSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Section.css";
+
+const BlackSection = () => {
+  return (
+    <section className="section black-section">
+      <h1>Welcome to the Black Section</h1>
+    </section>
+  );
+};
+
+export default BlackSection;
---
+++ src/components/WhiteSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Section.css";
+
+const WhiteSection = () => {
+  return (
+    <section className="section white-section">
+      <h1>Welcome to the White Section</h1>
+    </section>
+  );
+};
+
+export default WhiteSection;
